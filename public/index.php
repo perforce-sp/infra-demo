23cf06e4920654b17b63097117e2c2cb25fe8079
Stanislav Pääslane <spaaslane@perforce.com>
2025-01-22 15:32:36 +0200

    Test-1

--- public/index.php
+++ public/index.php
@@ -1,9 +1,20 @@
 <?php
 
-print '</br>';
+$env =
+    array_merge(
+        (parse_ini_file('../.env') ? parse_ini_file('../.env') : []),
+        (parse_ini_file('../.env.local') ? parse_ini_file('../.env.local') : [])
+    );
+
 print getenv('APP_NAME') ?: 'No var';
 print '</br>';
 print getenv('APP_ENV') ?: 'No var';
 print '</br>';
 print getenv('APP_SECRET') ?: 'No var';
+print '</br>';
+print $env['APP_NAME'] ?: 'No var';
+print '</br>';
+print $env['APP_ENV'] ?: 'No var';
+print '</br>';
+print $env['APP_SECRET'] ?: 'No var';
 print '</br>';
